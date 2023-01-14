c3cc9c87fcb147d88c9dd1f7b3bf0d75a1d73301
Robert Pendergrast <121700465+RobertPendergrast@users.noreply.github.com>
2023-01-14 11:46:26 -0500

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'IAmAwesome.com',
+    title: 'RobertPendergrast',
     htmlAttrs: {
       lang: 'en'
     },
